ac969f7e7b163cec2a3fc0c35ab94ee46349e65a
Emmanuel Sanchez <borderlinee97@gmail.com>
2026-05-23 14:39:09 -0600

    feat: correccion de proyecto de Chiapas a Puebla

--- src/components/MapPage.vue
+++ src/components/MapPage.vue
@@ -8,7 +8,7 @@
       <select v-model="selectedProject" @change="handleProjectChange">
         <option value="JALISCO">Jalisco</option>
         <option value="GUERRERO">Guerrero</option>
-        <option value="CHIAPAS">Chiapas</option>
+        <option value="CHIAPAS">Puebla</option>
       </select>
     </div>
 
